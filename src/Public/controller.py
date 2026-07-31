65c56f676c5f2792d31d343333ce505efa508153
Vivek Anand <va691187@gmail.com>
2026-08-01 00:08:54 +0530

    Fix error of date

--- src/Public/controller.py
+++ src/Public/controller.py
@@ -4,6 +4,7 @@ from src.Public.schema import InquirySchema
 from fastapi.encoders import jsonable_encoder
 from src.Public.email_service import send_email, send_admin_email
 from datetime import date, timedelta
+from bson import ObjectId
 
 # ========== Send Enquiry =========
 async def send_enquiry(data:InquirySchema):
@@ -12,11 +13,12 @@ async def send_enquiry(data:InquirySchema):
         "email": data.email,
         "phone": data.phone,
         "msg": data.msg,
-        "time": date.today()
+        "time": date.today().isoformat()
     }
     await enquireCollection.insert_one(new_enquiry)
     await send_email([data.email], data)
     await send_admin_email(data)
-    return {
-        "msg":"Mail Send Successfully"
-    }
+    return jsonable_encoder(
+        {"msg":"Mail Send Successfully"},
+        custom_encoder={ObjectId:str}
+    )
