da618ff72f22883a77245f136919bbbe9d2118fd
Vivek Anand <va691187@gmail.com>
2026-08-02 12:41:55 +0530

    Solving send inquiry

--- src/Public/controller.py
+++ src/Public/controller.py
@@ -8,17 +8,20 @@ from bson import ObjectId
 
 # ========== Send Enquiry =========
 async def send_enquiry(data:InquirySchema):
-    new_enquiry = {
-        "name": data.name,
-        "email": data.email,
-        "phone": data.phone,
-        "msg": data.msg,
-        "time": date.today().isoformat()
-    }
-    await enquireCollection.insert_one(new_enquiry)
-    await send_email([data.email], data)
-    await send_admin_email(data)
-    return jsonable_encoder(
-        {"msg":"Mail Send Successfully"},
-        custom_encoder={ObjectId:str}
-    )
+    try:
+        new_enquiry = {
+            "name": data.name,
+            "email": data.email,
+            "phone": data.phone,
+            "msg": data.msg,
+            "time": date.today().isoformat()
+        }
+        await enquireCollection.insert_one(new_enquiry)
+        await send_email([data.email], data)
+        await send_admin_email(data)
+        return jsonable_encoder(
+            {"msg":"Mail Send Successfully"},
+            custom_encoder={ObjectId:str}
+        )
+    except Exception as e:
+        raise HTTPException(500, detail=str(e))
